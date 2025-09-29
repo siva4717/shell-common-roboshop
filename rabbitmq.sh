e3d2da34565bd010f2ab20784ab69689404dea0f
siva4717 <sivagopi@4717>
2025-09-29 21:25:41 +0530

    shell-common-roboshop

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -3,7 +3,7 @@ source ./common.sh
 app_name=rabbitmq
 root_check
 
-cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$FILE_LOG
+cp $script_dir/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$FILE_LOG
 VALIDATE $? "Adding RabbitMQ repo"
 dnf install rabbitmq-server -y &>>$FILE_LOG
 VALIDATE $? "Installing RabbitMQ Server"
