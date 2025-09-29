5b41531fd453b41d487d13cfc495f77cfbf0538e
siva4717 <sivagopi@4717>
2025-09-29 21:25:10 +0530

    shell-common-roboshop

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -3,15 +3,15 @@ source ./common.sh
 app_name=rabbitmq
 root_check
 
-cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$FILE_LOG
 VALIDATE $? "Adding RabbitMQ repo"
-dnf install rabbitmq-server -y &>>$LOG_FILE
+dnf install rabbitmq-server -y &>>$FILE_LOG
 VALIDATE $? "Installing RabbitMQ Server"
 
 systemd_restart
 
-rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
+rabbitmqctl add_user roboshop roboshop123 &>>$FILE_LOG
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$FILE_LOG
 VALIDATE $? "Setting up permissions"
 
 script_running_time
