16d78cfcb01bf47c338af6ac9049687a4905b955
siva4717 <sivagopi@4717>
2025-09-29 22:59:03 +0530

    shell-common-roboshop

--- mysql.sh
+++ mysql.sh
@@ -1,16 +1,18 @@
 #?/bin/bash
 source ./common.sh
 root_check
-app_name=mysqld
 
 
 dnf install mysql-server -y &>>$FILE_LOG
 VALIDATE $? "install mysqld" 
 
-systemd_restart
+systemctl enable mysqld  &>>$FILE_LOG
+VALIDATE $? "enable mysqld"
+
+systemctl start mysqld   &>>$FILE_LOG
+VALIDATE $? "start mysqld" 
 
 mysql_secure_installation --set-root-pass RoboShop@1  &>>$FILE_LOG
 VALIDATE $? "mysql_secure_installation"
-
 
 script_running_time
